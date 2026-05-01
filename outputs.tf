3aecfb291cd89f26df71141f783cc7d482df7687
YusufS44 <yusuf.sadruddin@gmail.com>
2026-05-01 19:19:18 -0400

    Initial commit: VPC architecture and local-file setup

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "vpc_id" {
     description = "The ID of the VPC network"
     value = google_compute_network.vpc_network.id
 }
+
+output "my_favorite_food" {
+    description = "My favorite food is my homemade chili."
+    value = var.favorite_food
+}
